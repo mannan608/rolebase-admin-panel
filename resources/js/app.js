0d1ab6d0ab2b1f72d09e47aabc92ca96385a0e08
mannanswe <147591955+mannanswe@users.noreply.github.com>
2026-01-27 09:57:30 +0600

    project setup

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import './bootstrap';
+import Alpine from 'alpinejs';
+import ApexCharts from 'apexcharts';
+
+// flatpickr
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+// FullCalendar
+import { Calendar } from '@fullcalendar/core';
+
+
+
+window.Alpine = Alpine;
+window.ApexCharts = ApexCharts;
+window.flatpickr = flatpickr;
+window.FullCalendar = Calendar;
+
+Alpine.start();
+
+// Initialize components on DOM ready
+document.addEventListener('DOMContentLoaded', () => {
+    // Map imports
+   
+});
